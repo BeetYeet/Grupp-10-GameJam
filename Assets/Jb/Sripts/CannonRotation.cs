7b9587dd561c69f36104f515fbd3143fff985ee5
Jakob Bulow <jakobbulow01@gmail.com>
2019-05-28 18:02:24 +0200

    Merchant åker runt, Den andra som jag glömt namnet på åker rakt fram tills dens collider är trigger me player, då roterar den runt

---
+++ Assets/Jb/Sripts/CannonRotation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonRotation : MonoBehaviour
+{
+    [Range(0,3)]
+    public float speed = 2;
+    public float startRot;
+    public float endRot;
+    float t = 0;
+    bool fucker = true;
+    private void Start()
+    {
+        endRot = startRot + transform.eulerAngles.z;
+        startRot -= transform.eulerAngles.z;
+        if (startRot <= 0)
+        {
+            startRot = Mathf.Abs(startRot);
+        }
+        if (transform.eulerAngles.z >= 180)
+        {
+            float temp = startRot;
+            startRot = endRot;
+            endRot = temp;
+        }
+    }
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, Mathf.Lerp(startRot, endRot, t));
+            t += speed * Time.deltaTime;
+
+            if (t >= 1)
+            {
+                float temp = startRot;
+                startRot = endRot;
+                endRot = temp;
+                t = 0;
+                //Destroy(clone);
+            }
+        }
+    }
+}
---
+++ Assets/Jb/Sripts/SimpleEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemy : MonoBehaviour
+{
+    public SimpleEnemyMoveStats enemyMoveStats;
+    float endRot = 30;
+    float startRot;
+    private void Start()
+    {
+        StartCoroutine(changeDir());
+    }
+    private void Update()
+    {
+        transform.Translate(new Vector3(0, enemyMoveStats.speedForward * Time.deltaTime));
+    }
+    IEnumerator changeDir()
+    {
+        endRot = transform.eulerAngles.z + enemyMoveStats.anglesPerChange;
+        startRot = transform.eulerAngles.z;
+        float t = 0;
+        while (t < 1)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, Mathf.Lerp(startRot, endRot, t));
+            t += enemyMoveStats.rotSpeed * Time.deltaTime;
+            yield return null;
+        }
+        yield return new WaitForSeconds(enemyMoveStats.changeDirTime);
+        StartCoroutine(changeDir());
+    }
+}
